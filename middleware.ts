3897b935bc13f9ddb67a9bc768373fcdf5e8c20d
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-07-14 01:59:02 +0600

    type added in middleware function

--- middleware.ts
+++ middleware.ts
@@ -2,26 +2,30 @@ import { NextResponse } from "next/server";
 import { match } from "@formatjs/intl-localematcher";
 import Negotiator from "negotiator";
 
-const locales = process.env.LOCALES
+import type { NextRequest } from "next/server";
+
+const locales: string[] | undefined = process.env.LOCALES
   ? process.env.LOCALES.split(",")
   : undefined;
 
 let defaultLocale = "en";
 
-function getLocale(request: Request) {
+function getLocale(request: NextRequest) {
   const acceptLanguage = request.headers.get("accept-Language") ?? undefined;
 
   const headers = { "accept-language": acceptLanguage };
 
   const lenguages = new Negotiator({ headers }).languages();
 
+  if (!locales) return defaultLocale;
+
   return match(lenguages, locales, defaultLocale);
 }
 
-export function middleware(request: Request) {
+export function middleware(request: NextRequest) {
   // Check if there is any supported locale in the pathname
   const { pathname } = request.nextUrl;
-  const pathnameHasLocale = locales.some(
+  const pathnameHasLocale = locales?.some(
     (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
   );
 
