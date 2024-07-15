d7ffeb8385c52a5cbb4394231080841a1907a343
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-07-15 13:54:33 +0600

    page title layout fixed

--- app/[lang]/(home)/page.tsx
+++ app/[lang]/(home)/page.tsx
@@ -11,11 +11,11 @@ export default async function Home({ params: { lang = "en" } }: ParamProps) {
 
   return (
     <main className="max-w-[1276px] mx-auto  dark:text-[#bcccdf] pb-10 progress-bar">
-      <div className=" lg:flex flex-col hidden justify-center items-center p-10">
-        <h1 className="text-[34px] text-[#e49733] dark:text-[#38bdf8]">
+      <div className=" flex flex-col  justify-center items-center p-10">
+        <h1 className="text-2xl sm:text-[34px] text-[#e49733] dark:text-[#38bdf8]">
           {dictionary?.title}
         </h1>
-        <h4 className="text-[20px]"> {dictionary?.sub_title} </h4>
+        <h4 className=" sm:text-[20px]"> {dictionary?.sub_title} </h4>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3  m-auto">
         {data?.map((data, index) => {
